987028fc946dd7fb981f3242fd129922bd444526
DESKTOP-E94L0MO\Safeguard <saucxs@gmail.com>
2017-05-03 10:54:14 +0800

    修改read.me，以及修改插件使用demo

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # watermark
-水印插件，包含1、水印插件，2、水印插件的测试demo，3、read.me
 
+`watermark.js`是一个给B/S网站系统加一个很浅的水印插件，确保系统的保密性，安全性。
+
+水印插件内容，包含1、水印插件-使用，2、水印插件-测试demo，3、API介绍
 
 #1、水印插件-使用
 
 只是简单的加一个很浅的水印，实现起来很容易。不需要引入jquery插件。
 
 `watermark.js`是必须要引进的组件
+
+获取方式：`git clone https://github.com/saucxs/watermark.git`
 
 在项目`javascript`中引入`watermark.js`
 
@@ -15,29 +19,25 @@
         		       <script>watermark.load({ watermark_txt: "测试水印，1021002301，测试水印，100101010111101" })</script>
          <!--watermark end-->
 
-既可以看到效果，水印插件使用demo地址
+就可以看到效果，水印插件使用demo地址
 
-http://www.chengxinsong.cn/sinaweibo/
+使用插件的效果地址：http://www.chengxinsong.cn/sinaweibo/
 
 获取一个watermark_demo1的源码（watermark_demo1文件夹）
 
-`git clone https://github.com/saucxs/watermark.git`
-
-
-
 #2、水印插件-测试demo
 
-`git clone https://github.com/saucxs/watermark.git`
+获取方式：`git clone https://github.com/saucxs/watermark.git`
 
 用浏览器打开index.html
 
-测试地址：http://www.chengxinsong.cn/watermark_test
+效果地址：http://www.chengxinsong.cn/watermark_test
+
+项目中使用插件的步骤：
 
 （1）先下载watermark.js和index.js
 
 （2）从copy start到copy end中间部分复制，粘贴在项目需要用到的页面中。
-
-（3）页面中引入这2个js文件
 
     <!--copy start-->
     <div class="wm_box" style="position: fixed;top:5px; left: 5px; z-index: 100;">
@@ -108,3 +108,54 @@ http://www.chengxinsong.cn/sinaweibo/
     <script src="watermark.js"></script>
     <script src="index.js"></script>
     <!--copy end-->
+    
+（3）就可以看到水印插件-测试demo的效果
+
+
+
+#3、API介绍
+    
+ 格式：[请求类型:]URL地址
+ 
+ 例如：
+ 
+     get:/api/watermark    //获取水印参数
+     post:/api/watermark   //发布水印参数
+     put:/api/watermark    //更新水印参数
+     delete:/api/watermark   //删除水印参数
+     
+ PS：如果没有添加请求类型的话，默认为get请求。
+ 
+ 返回结果定义：
+ 
+ 返回结果根据不同的状态需要定义，返回结果`<<<`标识，例如：
+ 
+    <<<
+    success
+    {
+        "errNum":0,
+        "retMsg":"success",
+        "retData":{
+              "watermark_txt":"测试水印",
+              "watermark_x":20,//水印起始位置x轴坐标
+              "watermark_y":20,//水印起始位置Y轴坐标
+              "watermark_rows":0,//水印行数
+              "watermark_cols":0,//水印列数
+              "watermark_x_space":100,//水印x轴间隔
+              "watermark_y_space":50,//水印y轴间隔
+              "watermark_font":'微软雅黑',//水印字体
+              "watermark_color":'black',//水印字体颜色
+              "watermark_fontsize":'18px',//水印字体大小
+              "watermark_alpha":0.15,//水印透明度，要求设置在大于等于0.003
+              "watermark_width":150,//水印宽度
+              "watermark_height":100,//水印长度
+              "watermark_angle":15,//水印倾斜度数
+        }
+    }
+    <<<
+    error
+    {
+        "errNum":0,
+        "retMsg":"success",
+        "retData":[]
+    }
--- watermark_demo1/watermark.js
+++ watermark_demo1/watermark.js
@@ -1,121 +1,125 @@
 (function(watermark){
-	window.watermarkdivs = [];
-	// 加载水印
-	var loadMark = function(settings) {
-			var defaultSettings={
-				watermark_txt:"text",
-				watermark_x:20,//水印起始位置x轴坐标
-				watermark_y:20,//水印起始位置Y轴坐标
-				watermark_rows:0,//水印行数
-				watermark_cols:0,//水印列数
-				watermark_x_space:100,//水印x轴间隔
-				watermark_y_space:50,//水印y轴间隔
-				watermark_color:'#000000',//水印字体颜色
-				watermark_alpha:0.05,//水印透明度，要求设置在大于等于0.005
-				watermark_fontsize:'18px',//水印字体大小
-				watermark_font:'微软雅黑',//水印字体
-				watermark_width:150,//水印宽度
-				watermark_height:100,//水印长度
-				watermark_angle:15,//水印倾斜度数
-				watermark_bg_alpha:0.5 //加透明度导致隐形水印无法读取
-			};
-			//采用配置项替换默认值，作用类似jquery.extend
-			if(arguments.length===1&&typeof arguments[0] ==="object" )
-			{
-				var src=arguments[0]||{};
-				for(key in src)
-				{
-					if(src[key]&&defaultSettings[key]&&src[key]===defaultSettings[key])
-						continue;
-					else if(src[key])
-						defaultSettings[key]=src[key];
-				}
-			}
+    window.watermarkdivs = [];
+    // 加载水印
+    var loadMark = function(settings) {
+        var defaultSettings={
+            watermark_txt:"测试水印",
+            watermark_x:20,//水印起始位置x轴坐标
+            watermark_y:20,//水印起始位置Y轴坐标
+            watermark_rows:0,//水印行数
+            watermark_cols:0,//水印列数
+            watermark_x_space:100,//水印x轴间隔
+            watermark_y_space:50,//水印y轴间隔
+            watermark_font:'微软雅黑',//水印字体
+            watermark_color:'black',//水印字体颜色
+            watermark_fontsize:'18px',//水印字体大小
+            watermark_alpha:0.15,//水印透明度，要求设置在大于等于0.003
+            watermark_width:150,//水印宽度
+            watermark_height:100,//水印长度
+            watermark_angle:15,//水印倾斜度数
+        };
+        console.log(defaultSettings);
+        //采用配置项替换默认值，作用类似jquery.extend
+        if(arguments.length===1&&typeof arguments[0] ==="object" )
+        {
+            var src=arguments[0]||{};
+            for(key in src)
+            {
+                if(src[key]&&defaultSettings[key]&&src[key]===defaultSettings[key])
+                    continue;
+                else if(src[key])
+                    defaultSettings[key]=src[key];
+            }
+        }
 
-			if (window.watermarkdivs && window.watermarkdivs.length > 0) {
-				document.body.removeChild(document.getElementById("otdivid"));
-				window.watermarkdivs = [];
-			}
+        if (window.watermarkdivs && window.watermarkdivs.length > 0) {
+            document.body.removeChild(document.getElementById("otdivid"));
+            window.watermarkdivs = [];
+        }
 
-			//获取页面最大宽度
-			var page_width = Math.max(document.body.scrollWidth,document.body.clientWidth);
-			//获取页面最大长度
-			var page_height = Math.max(document.body.scrollHeight,document.body.clientHeight);
+        //获取页面最大宽度
+        var page_width = Math.max(document.body.scrollWidth,document.body.clientWidth);
+        //获取页面最大长度
+        var page_height = Math.max(document.body.scrollHeight,document.body.clientHeight);
 
-        	// 创建文档碎片
-        	var oTemp = document.createDocumentFragment();
-            //创建水印外壳div
-			var otdiv = document.getElementById("otdivid");
-			if(!otdiv){
-				otdiv =document.createElement('div');
-				otdiv.id="otdivid";
-                otdiv.style.pointerEvents = "none";
-				document.body.appendChild(otdiv);
-			}
+        // 创建文档碎片
+        var oTemp = document.createDocumentFragment();
+        //创建水印外壳div
+        var otdiv = document.getElementById("otdivid");
+        if(!otdiv){
+            otdiv =document.createElement('div');
+            otdiv.id="otdivid";
+            otdiv.style.pointerEvents = "none";
+            document.body.appendChild(otdiv);
+        }
 
-			//如果将水印列数设置为0，或水印列数设置过大，超过页面最大宽度，则重新计算水印列数和水印x轴间隔
-			if (defaultSettings.watermark_cols == 0 || (parseInt(defaultSettings.watermark_x + defaultSettings.watermark_width *defaultSettings.watermark_cols + defaultSettings.watermark_x_space * (defaultSettings.watermark_cols - 1)) > page_width)) {
-				defaultSettings.watermark_cols = parseInt((page_width - defaultSettings.watermark_x + defaultSettings.watermark_x_space) / (defaultSettings.watermark_width + defaultSettings.watermark_x_space));
-				defaultSettings.watermark_x_space = parseInt((page_width - defaultSettings.watermark_x - defaultSettings.watermark_width * defaultSettings.watermark_cols) / (defaultSettings.watermark_cols - 1));
-			}
-			//如果将水印行数设置为0，或水印行数设置过大，超过页面最大长度，则重新计算水印行数和水印y轴间隔
-			if (defaultSettings.watermark_rows == 0 || (parseInt(defaultSettings.watermark_y + defaultSettings.watermark_height * defaultSettings.watermark_rows + defaultSettings.watermark_y_space * (defaultSettings.watermark_rows - 1)) > page_height)) {
-				defaultSettings.watermark_rows = parseInt((defaultSettings.watermark_y_space + page_height - defaultSettings.watermark_y) / (defaultSettings.watermark_height + defaultSettings.watermark_y_space));
-				defaultSettings.watermark_y_space = parseInt(((page_height - defaultSettings.watermark_y) - defaultSettings.watermark_height * defaultSettings.watermark_rows) / (defaultSettings.watermark_rows - 1));
-			}
-			
-			var x;
-			var y;
-			for (var i = 0; i < defaultSettings.watermark_rows; i++) {
-				y = defaultSettings.watermark_y + (defaultSettings.watermark_y_space + defaultSettings.watermark_height) * i;
-				for (var j = 0; j < defaultSettings.watermark_cols; j++) {
-					x = defaultSettings.watermark_x + (defaultSettings.watermark_width + defaultSettings.watermark_x_space) * j;
+        //如果将水印列数设置为0，或水印列数设置过大，超过页面最大宽度，则重新计算水印列数和水印x轴间隔
+        if (defaultSettings.watermark_cols == 0 || (parseInt(defaultSettings.watermark_x + defaultSettings.watermark_width *defaultSettings.watermark_cols + defaultSettings.watermark_x_space * (defaultSettings.watermark_cols - 1)) > page_width)) {
+            defaultSettings.watermark_cols = parseInt((page_width - defaultSettings.watermark_x + defaultSettings.watermark_x_space) / (defaultSettings.watermark_width + defaultSettings.watermark_x_space));
+            defaultSettings.watermark_x_space = parseInt((page_width - defaultSettings.watermark_x - defaultSettings.watermark_width * defaultSettings.watermark_cols) / (defaultSettings.watermark_cols - 1));
+        }
+        //如果将水印行数设置为0，或水印行数设置过大，超过页面最大长度，则重新计算水印行数和水印y轴间隔
+        if (defaultSettings.watermark_rows == 0 || (parseInt(defaultSettings.watermark_y + defaultSettings.watermark_height * defaultSettings.watermark_rows + defaultSettings.watermark_y_space * (defaultSettings.watermark_rows - 1)) > page_height)) {
+            defaultSettings.watermark_rows = parseInt((defaultSettings.watermark_y_space + page_height - defaultSettings.watermark_y) / (defaultSettings.watermark_height + defaultSettings.watermark_y_space));
+            defaultSettings.watermark_y_space = parseInt(((page_height - defaultSettings.watermark_y) - defaultSettings.watermark_height * defaultSettings.watermark_rows) / (defaultSettings.watermark_rows - 1));
+        }
 
-					var mask_div = document.createElement('div');
-					var oText=document.createTextNode(defaultSettings.watermark_txt);
-					mask_div.appendChild(oText);
-             // 设置一张水印相关属性start
-                    mask_div.id = 'mask_div' + i + j;
-					//设置水印div倾斜显示
-					mask_div.style.webkitTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
-					mask_div.style.MozTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
-					mask_div.style.msTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
-					mask_div.style.OTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
-					mask_div.style.transform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
-					mask_div.style.visibility = "";
-					mask_div.style.position = "absolute";
-					//选不中
-					mask_div.style.left = x + 'px';
-					mask_div.style.top = y + 'px';
-					mask_div.style.overflow = "hidden";
-					mask_div.style.zIndex = "9999";
-					//mask_div.style.border="solid #eee 1px";
-					mask_div.style.opacity = defaultSettings.watermark_alpha;
-					mask_div.style.fontSize = defaultSettings.watermark_fontsize;
-					mask_div.style.fontFamily = defaultSettings.watermark_font;
-					mask_div.style.color = defaultSettings.watermark_color;
-					mask_div.style.textAlign = "center";
-					mask_div.style.width = defaultSettings.watermark_width + 'px';
-					mask_div.style.height = defaultSettings.watermark_height + 'px';
-					mask_div.style.display = "block";
-             //设置一张水印相关属性end
-                    //附加到文档碎片中
-                    otdiv.appendChild(mask_div);
+        var x;
+        var y;
+        for (var i = 0; i < defaultSettings.watermark_rows; i++) {
+            y = defaultSettings.watermark_y + (defaultSettings.watermark_y_space + defaultSettings.watermark_height) * i;
+            for (var j = 0; j < defaultSettings.watermark_cols; j++) {
+                x = defaultSettings.watermark_x + (defaultSettings.watermark_width + defaultSettings.watermark_x_space) * j;
 
-                    window.watermarkdivs.push(otdiv); //控制页面大小变化时水印字体
-				};
-			};
-			//一次性添加到document中
-			document.body.appendChild(oTemp);
-		};
-	
-	watermark.load = function(settings) {
-		window.onload = function() {
-			loadMark(settings);
-		};
-		window.onresize = function() {
-			loadMark(settings);
-		};
-	};
-	
-})(window.watermark = {});
+                var mask_div = document.createElement('div');
+                var oText=document.createTextNode(defaultSettings.watermark_txt);
+                mask_div.appendChild(oText);
+                // 设置水印相关属性start
+                mask_div.id = 'mask_div' + i + j;
+                //设置水印div倾斜显示
+                mask_div.style.webkitTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
+                mask_div.style.MozTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
+                mask_div.style.msTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
+                mask_div.style.OTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
+                mask_div.style.transform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
+                mask_div.style.visibility = "";
+                mask_div.style.position = "absolute";
+                //选不中
+                mask_div.style.left = x + 'px';
+                mask_div.style.top = y + 'px';
+                mask_div.style.overflow = "hidden";
+                mask_div.style.zIndex = "9999";
+                //mask_div.style.border="solid #eee 1px";
+                mask_div.style.opacity = defaultSettings.watermark_alpha;
+                mask_div.style.fontSize = defaultSettings.watermark_fontsize;
+                mask_div.style.fontFamily = defaultSettings.watermark_font;
+                mask_div.style.color = defaultSettings.watermark_color;
+                mask_div.style.textAlign = "center";
+                mask_div.style.width = defaultSettings.watermark_width + 'px';
+                mask_div.style.height = defaultSettings.watermark_height + 'px';
+                mask_div.style.display = "block";
+                //设置水印相关属性end
+                //附加到文档碎片中
+                otdiv.appendChild(mask_div);
+
+                window.watermarkdivs.push(otdiv); //控制页面大小变化时水印字体
+            };
+        };
+        //一次性添加到document中
+        document.body.appendChild(oTemp);
+    };
+
+    watermark.init = function(settings) {
+        window.onload = function() {
+            loadMark(settings);
+        };
+        window.onresize = function() {
+            loadMark(settings);
+        };
+    };
+
+    watermark.load = function(settings){
+        loadMark(settings);
+    };
+
+})(window.watermark = {} );
