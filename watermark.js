5d767d368d7c7ea3547bab00e10e28ba86a881bd
mrbean <xakep@live.cn>
2018-05-08 17:10:37 +0800

    支持IE6、7、8、9、10

--- watermark.js
+++ watermark.js
@@ -17,7 +17,7 @@
             watermark_alpha:0.15,          //水印透明度，要求设置在大于等于0.003
             watermark_width:150,           //水印宽度
             watermark_height:100,          //水印长度
-            watermark_angle:15,            //水印倾斜度数
+            watermark_angle:15             //水印倾斜度数
         };
 
         //采用配置项替换默认值，作用类似jquery.extend
@@ -45,9 +45,19 @@
         var page_width = Math.max(document.body.scrollWidth,document.body.clientWidth)-30;
         //获取页面最大长度
         var page_height = Math.max(document.body.scrollHeight,document.body.clientHeight)-30;
-
-        // 创建文档碎片
-        var oTemp = document.createDocumentFragment();
+        
+        // 是否支持ShadowRoot
+        var IsShadowRoot = typeof document.createShadowRoot === 'function';
+        //var IsIE78=/msie [6|7|8]/i.test(window.navigator.userAgent);
+        //var cosv=0;
+        //var sinv=0;
+        //var M12=0;
+        //if(IsIE78){
+        //    var fv=360-defaultSettings.watermark_angle%360;
+        //    cosv=Math.cos(fv);
+        //    sinv=Math.sin(fv);
+        //    M12=-sinv;
+        //}
 
         //创建水印外壳div
         var otdiv = document.getElementById(defaultSettings.watermark_id);
@@ -93,8 +103,7 @@
                 x = defaultSettings.watermark_x + (defaultSettings.watermark_width + defaultSettings.watermark_x_space) * j;
 
                 var mask_div = document.createElement('div');
-                var oText=document.createTextNode(defaultSettings.watermark_txt);
-                mask_div.appendChild(oText);
+                mask_div.innerHTML = defaultSettings.watermark_txt;
                 // 设置水印相关属性start
                 mask_div.id = defaultSettings.watermark_id + i + j;
                 //设置水印div倾斜显示
@@ -110,8 +119,10 @@
                 mask_div.style.top = y + 'px';
                 mask_div.style.overflow = "hidden";
                 mask_div.style.zIndex = "9999";
-                //mask_div.style.border="solid #eee 1px";
                 mask_div.style.opacity = defaultSettings.watermark_alpha;
+                mask_div.style.filter="progid:DXImageTransform.Microsoft.Alpha(Opacity="+(defaultSettings.watermark_alpha*100)+")";
+                //if(IsIE78) //ie7 8
+                //    mask_div.style.filter="progid:DXImageTransform.Microsoft.Matrix(M11="+cosv+",M12="+M12+",M21="+sinv+",M22="+cosv+",SizingMethod='auto expand')progid:DXImageTransform.Microsoft.Alpha(Opacity="+(defaultSettings.watermark_alpha*100)+")";
                 mask_div.style.fontSize = defaultSettings.watermark_fontsize;
                 mask_div.style.fontFamily = defaultSettings.watermark_font;
                 mask_div.style.color = defaultSettings.watermark_color;
@@ -119,27 +130,121 @@
                 mask_div.style.width = defaultSettings.watermark_width + 'px';
                 mask_div.style.height = defaultSettings.watermark_height + 'px';
                 mask_div.style.display = "block";
+                if(!IsShadowRoot)
+                {
+                    mask_div.onclick= function(){
+                        var vcliEle = getElementsByClientXY(document, event.clientX, event.clientY, 0, 0);
+                        if (vcliEle != null) {
+                            switch (vcliEle.nodeName) {
+                                case "A": 
+                                    vcliEle.click();
+                                    break;
+                                case "SELECT":
+                                    //找不到方法打开下拉框
+                                    vcliEle.focus();
+                                    break;
+                                case "TEXTAREA":
+                                    vcliEle.focus();
+                                    break;
+                                case "INPUT":
+                                    var vtype = vcliEle.getAttribute("type").toLowerCase();
+                                    switch (vtype) {
+                                        case "radio":
+                                        case "checkbox":
+                                        case "button":
+                                        case "submit":
+                                        case "reset":
+                                        case "file":
+                                            vcliEle.click();
+                                            break;
+                                        default:
+                                            vcliEle.focus();
+                                            break;
+                                    }
+                                    break;
+                            }
+                        }
+                    };
+                }
                 //设置水印相关属性end
                 //附加到文档碎片中
                 shadowRoot.appendChild(mask_div);
             };
         };
     };
+
+    var getXYTagByTagName = function (tagname, doc, clientX, clientY, fleft, ftop) {
+        //本级的控件
+        var inputs = doc.getElementsByTagName(tagname);
+        for (var i = 0, j = inputs.length; i < j; i++) {
+            var vrect = inputs[i].getBoundingClientRect();
+            var xmin = vrect.left + fleft;
+            var ymin = vrect.top + ftop;
+            var vwidth = vrect.width || inputs[i].offsetWidth;
+            var vheight = vrect.height || inputs[i].clientHeight;
+            var xmax = xmin + vwidth;
+            var ymax = ymin + vheight;
+            
+            if (xmin <= clientX && xmax >= clientX && ymin <= clientY && ymax >= clientY) {
+                return inputs[i];
+            }
+        }
+        return null;
+    }
+
+    var getElementsByClientXY = function (doc, clientX, clientY, fleft, ftop) {
+        //本级的控件
+        var vtag = getXYTagByTagName("input",doc, clientX, clientY, fleft, ftop);
+        if(vtag!=null)
+            return vtag;
+
+        vtag = getXYTagByTagName("textarea",doc, clientX, clientY, fleft, ftop);
+        if(vtag!=null)
+            return vtag;
+        vtag = getXYTagByTagName("select",doc, clientX, clientY, fleft, ftop);
+        if(vtag!=null)
+            return vtag;
+
+        vtag = getXYTagByTagName("a",doc, clientX, clientY, fleft, ftop);
+        if(vtag!=null)
+            return vtag;
+
+        //检查是否有iframe
+        var viframe = doc.getElementsByTagName("iframe");
+        for (var k = 0, m = viframe.length; k < m; k++) {
+            var vfrect = viframe[k].getBoundingClientRect();
+            vfleft = fleft + vfrect.left;
+            vftop = ftop + vfrect.top;
+            var vframedoc = viframe[k].contentDocument || viframe[k].contentWindow.document;
+            var ifrele = getElementsByClientXY(vframedoc, clientX, clientY, vfleft, vftop);
+            if (ifrele != null)
+                return ifrele;
+        }
+
+        return null;
+    }
+
     //初始化水印，添加load和resize事件
-    watermark.init = function(settings) {
-        window.addEventListener('load',function () {
+    watermark.init = function (settings) {
+        try {
+            window.addEventListener('load', function () {
+                loadMark(settings);
+            });
+            window.addEventListener('resize', function () {
+                loadMark(settings);
+            });
+            window.addEventListener('DOMContentLoaded', function () {
+                loadMark(settings);
+            })
+        }
+        catch (ex)
+        {
             loadMark(settings);
-        });
-        window.addEventListener('resize',function () {
-            loadMark(settings);
-        });
-        window.addEventListener('DOMContentLoaded',function () {
-            loadMark(settings);
-        })
+        }
     };
     //手动加载水印
-    watermark.load = function(settings){
+    watermark.load = function (settings) {
         loadMark(settings);
     };
 
-})(window.watermark = {} );
+})(window.watermark = {});
